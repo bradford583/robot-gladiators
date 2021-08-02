f046c2c3ea868912e8591b7be96c8c76a4c63ae7
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-08-01 21:59:40 -0600

    added some initial console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("this is an alert! JavaScript is running!")
+function fight () {
+    window.alert("The Fight has begun!")
+}
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a messge");
+
+console.log(10*10);
+
+console.log("Our robot's name is " + playerName);
+
+// fight();
