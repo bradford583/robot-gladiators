2a5c775d1847ffdb91e48a75003d9f210879fe94
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-08-02 23:40:46 -0600

    added rounds and a game over message

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,6 @@ var enemyNames = ["Roborto" , "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-window.alert("Welcome to Robot Gladiators!");
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd like to fight or run
@@ -68,6 +67,18 @@ var fight = function(enemyName) {
 
 for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        //let player know what round they are in, remember that arrays start at 0 so it needs to have a 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+
+    else {
+        window.alert("You have lost your robot in battle! Game Over!")
+    }
 }
