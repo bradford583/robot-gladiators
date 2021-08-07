903f1f5590eff910eb491d4040ca250362be6f4a
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-08-07 12:46:42 -0600

    randomized fighting order

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,7 @@ var randomNumber = function(min, max) {
 }
 
 var fightOrSkip = function() {
+  
   //ask player if they'd like to fight or skip using fightOr Skip function
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
         //Enter the conditional recursive function call here
@@ -25,52 +26,83 @@ var fightOrSkip = function() {
           playerInfo.money = Math.max(0, playerInfo.money - 10);
           console.log("playerMoney", playerInfo.money)
 }
-var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-      // ask player if they'd like to fight or run
-        fightOrSkip();
-        }
       }
-  
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
+    }
+var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
+
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+      // remove enemy's health by subtracting the amount we set in the damage variable
       enemy.health = Math.max(0, enemy.health - damage);
       console.log(
-        playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
+        playerInfo.name +
+          " attacked " +
+          enemy.name +
+          ". " +
+          enemy.name +
+          " now has " +
+          enemy.health +
+          " health remaining."
       );
-  
+
       // check enemy's health
       if (enemy.health <= 0) {
-        window.alert(enemy.name + ' has died!');
-  
+        window.alert(enemy.name + " has died!");
+
         // award player money for winning
         playerInfo.money = playerInfo.money + 20;
-  
+
         // leave while() loop since enemy is dead
         break;
       } else {
-        window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
+        window.alert(enemy.name + " still has " + enemy.health + " health left.");
       }
-  
-      // remove players's health by subtracting the amount set in the enemy.attack variable
+      // player gets attacked first
+    } else {
       var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
+      // remove player's health by subtracting the amount we set in the damage variable
       playerInfo.health = Math.max(0, playerInfo.health - damage);
       console.log(
-        enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health + ' health remaining.'
+        enemy.name +
+          " attacked " +
+          playerInfo.name +
+          ". " +
+          playerInfo.name +
+          " now has " +
+          playerInfo.health +
+          " health remaining."
       );
-  
+
       // check player's health
       if (playerInfo.health <= 0) {
-        window.alert(playerInfo.name + ' has died!');
+        window.alert(playerInfo.name + " has died!");
         // leave while() loop if player is dead
         break;
       } else {
-        window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
+        window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
       }
     }
-  };
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
+  }
+};
+
   // function to start a new game
   var startGame = function () {
     playerInfo.reset(); 
@@ -207,8 +239,5 @@ var fight = function(enemy) {
           attack: randomNumber(10, 14)
         }
       ];
-      
+    
       startGame();
-
-      
-      
