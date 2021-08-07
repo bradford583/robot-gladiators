b439aaea26ec0af8e7713eaffc9f36b077dbb23e
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-08-07 13:50:29 -0600

    jfinished highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,6 +25,7 @@ var fightOrSkip = function() {
           // subtract money from playerMoney for skipping
           playerInfo.money = Math.max(0, playerInfo.money - 10);
           console.log("playerMoney", playerInfo.money)
+          shop();
 }
       }
     }
@@ -136,18 +137,30 @@ var fight = function(enemy) {
       };
         
       var endGame = function() {
-        if (playerInfo.health > 0) {
-          window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("The game has now ended. Let's see how you did!");
+        
+        //check localStorage for hight score, if it's not there use 0
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+          highScore = 0;
+        }
+        //if player has more money than the highscore, player has new high score.
+        if (playerInfo.money > highscore) {
+          localStorage.setItem("highscore", playerInfo.money);
+          localStorage.setItem("name", playerInfo.name);
+
+          alert(playerInfo.name + " now has the high of " + playerInfo.money + "!");
         }
         else {
-          window.alert("You've lost your robot in battle.");
+          alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
         }
-
+      
+        // ask player if they'd like to play again
         var playAgainConfirm = window.confirm("Would you like to play again?");
-
+      
         if (playAgainConfirm) {
           startGame();
-        }
+        } 
         else {
           window.alert("Thank you for playing Robot Gladiators! Come back soon!");
         }
